b0af4c582a00eb6d984abcd8a4d3fe27463abefd
AlirezaNG-2020 <alirezanaghash2019@gmail.com>
2025-07-29 14:17:14 +0330

    Remove logs folder from git tracking

--- project-laravel-2/mini_project/resources/views/admin/alerts/error.blade.php
+++
@@ -1,6 +0,0 @@
-@if (session('alert-error'))
-    <div class="alert alert-danger" role="alert">
-        <i class="bi bi-ban"></i>
-        {{ session('alert-error') }}
-    </div>  
-@endif()
--- project-laravel-2/mini_project/resources/views/admin/layouts/script.blade.php
+++
@@ -1,2 +0,0 @@
-<script src="{{ asset('admin-assets/js/bootstrap/bootstrap.min.js') }}"></script>
-<script src="{{ asset('admin-assets/js/bootstrap/grid.js') }}"></script>
